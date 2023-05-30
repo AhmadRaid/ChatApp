150506094c3d797348c6ca33dd3940b434968f00
AhmadRaid <52861533+AhmadRaid@users.noreply.github.com>
2023-05-30 17:55:33 +0300

    solve error typezeft 13

--- utils/jwt.ts
+++ utils/jwt.ts
@@ -1,7 +1,7 @@
 const JWT = require("jsonwebtoken");
 require("dotenv").config();
 
-export function createAccessToken(object : object) {
+export function createAccessToken(object: object) {
   const token = JWT.sign(
     {
       data: object,
@@ -28,29 +28,12 @@ export function create_Tokens_with_cookie(object: object) {
     access_Token: createAccessToken(object),
     refresh_token: createRefreshToken(object),
   };
-
-  //   res.cookie('token', accessToken, {
-  //     expires: new Date(
-  //       Date.now() + process.env.TOKEN_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
-  //     ),
-  //     httpOnly: true,
-  //  //   secure: req.secure || req.headers['x-forwarded-proto'] === 'https'
-  //   });
-
-  //   res.cookie('refresh_token', refreshToken, {
-  //     expires: new Date(
-  //       Date.now() + process.env.REFRESH_TOKEN_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
-  //     ),
-  //     httpOnly: true,
-  //  //    secure: req.secure || req.headers['x-forwarded-proto'] === 'https'
-  //   });
 }
 
-export function verifyAccessToken(token : string) {
-  return JWT.verify(token, "JWT_SECRET_ACCESS_Token_KEY");
+export function verifyAccessToken(token: string) {
+  return JWT.verify(token, "Ahmad_Raid_Secret_Token_key");
 }
 
 export function verifyRefreshToken(token: string) {
   return JWT.verify(token, "Ahmad_Raid_Secret_Refresh_Token_key");
 }
-
